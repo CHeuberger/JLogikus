934163781debfe5de612f79c187f1dc2f503c577
Carlos Heuberger <2377128+CHeuberger@users.noreply.github.com>
2020-03-27 22:06:18 +0000

    #3 added GUI

--- src/java/cfh/jlogikus/GUI.java
+++ src/java/cfh/jlogikus/GUI.java
@@ -1,24 +1,26 @@
 package cfh.jlogikus;
 
+import java.util.Optional;
+
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-public class TestGUI {
-
-    private static final String VERSION = "0.0.2";
+public class GUI {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new TestGUI()::initGUI);
+        SwingUtilities.invokeLater(new GUI()::initGUI);
     }
     
+    private final String version;
     private JFrame frame;
     
-    private TestGUI() {
+    private GUI() {
+        version = Optional.ofNullable(getClass().getPackage().getImplementationVersion()).orElse("dev");
     }
     
     private void initGUI() {
         frame = new JFrame();
-        frame.setTitle("JLogikus " + VERSION + " by Carlos Heuberger") ;
+        frame.setTitle("JLogikus - " + version + " - by Carlos Heuberger") ;
         frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
         frame.add(new LogikusPanel());
         frame.pack();
